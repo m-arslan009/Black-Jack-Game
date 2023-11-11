526dd99d26d484cfa6c52be706fd2306cdf54c13
Muhammad Arslan <iqbalarslan009@gmail.com>
2023-11-11 18:39:49 +0500

    adding new function called and startGame() and rename the startGame() -> renderGame() as if we call again and again to startGame function it does not make any sense

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+
 function startGame() {
+    renderGame();
+}
+function renderGame() {
     cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
     if(sum < 21) {
         message = "Do you want to draw a new card?"
@@ -22,10 +26,13 @@ function startGame() {
         message = "You're out of the game!"
         isAlive = false;
     }
-    sumEl.textContent = sumEl.textContent + " " + sum;
+    sumEl.textContent = "Sum: " + " " + sum;
     messageEl.textContent = message
 }
 
 function newCard() {
     console.log("Drawing a new card from the deck!");
+    let card = 7;
+    sum += card;
+    renderGame();
 }
