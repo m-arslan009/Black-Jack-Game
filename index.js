011b46e3e94a612f69d5f7b0c720079bc9c8ea37
Muhammad Arslan <iqbalarslan009@gmail.com>
2023-12-09 10:49:08 +0500

    Adding a function of getRandomCard() and assign a values in side randerdGame()

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
+let firstCard;
+let secondCard;
+let cards = [];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
-
+console.log(cards);
 function getRandomCard() {
-    return 5;
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    // i am gona assign A card a number of 11 as well because it also has a value of 1 and jack queen and kind a value of 10;
+    if(randomNumber > 10) {
+        return 10;
+    } else if(randomNumber === 1) {
+        return 11;
+    } else {
+        return randomNumber;
+    }
 }
 
 function startGame() {
+    // i just define the values here as if defined at the top then before showing on the screen it shows on console what numbers a randomNumber() going to guess
+    isAlive = true
+    firstCard = getRandomCard()
+    secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame();
 }
 function renderGame() {
